e9c042dd31ab72c1a21ec7f0c3ccd342fe2ddb3f
Hannington Odhiambo <36677724+Tony2308@users.noreply.github.com>
2020-07-14 16:48:54 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -4,22 +4,23 @@ let lockUnlock = document.getElementById('lockUnlock');
 const date = new Date();
 let speechDate = date.getDate();
 let label = document.getElementById('label');
-let input = document.querySelector('input').nodeValue;
-var predictDay = +document.querySelector('#guessDay').value;
+let input = document.querySelector('#guess');
+
 
 
 //cupture date entered by the user as integer
 // let predictDay = +prompt('Guess Cessation Date:');
+// let predictDay = +document.querySelector('#guess').value;
+let predictDay = document.getElementById('guess').value;
 
+// let correctDate = parseInt(predictDay);
  // let predictDay = +document.getElementById('guessDate').value;
 
 function lockDown(){
-   
-
     lockUnlock.addEventListener( 'click', ()=> {
         
         if( speechDate === predictDay ){
-
+           
             input.style.display ='none';
             label.innerHTML = 'We are free to roam!!!!'
             lockUnlock.innerHTML = 'Lock';
@@ -35,7 +36,6 @@ function lockDown(){
 
     });
 
-    document.getElementById('guessDate').value = '';
 }
 
 lockDown();
